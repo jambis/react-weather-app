70519fa1f708ec4b7346e6d0ddfa9a4a67bceac0
jambis <imjaamees@gmail.com>
2019-03-01 15:09:14 -0600

    Created build to deply on github pages

---
+++ docs/precache-manifest.b3022546bd37dc924f328146e9dd7d01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "891b943967a5bcfaae23",
+    "url": "/static/css/main.7a2908a6.chunk.css"
+  },
+  {
+    "revision": "891b943967a5bcfaae23",
+    "url": "/static/js/main.891b9439.chunk.js"
+  },
+  {
+    "revision": "4a288950a4ff1d7d5133",
+    "url": "/static/js/1.4a288950.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "14de59be04985da617f89561fce696b7",
+    "url": "/index.html"
+  }
+];
